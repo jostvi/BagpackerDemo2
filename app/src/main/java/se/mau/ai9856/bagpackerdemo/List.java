3b542156da9a4175b74234aa855653cdf34b1b03
Johan Steen Wingren <steen81@gmail.com>
2019-05-27 10:49:06 +0200

    reverted last commit

--- app/src/main/java/se/mau/ai9856/bagpackerdemo/List.java
+++ app/src/main/java/se/mau/ai9856/bagpackerdemo/List.java
@@ -103,6 +103,6 @@ public class List {
 
         info = dest + "\n" + startDate + " - " + endDate + "\n" + "min: " + minTemp + " °C, max: "
                 + maxTemp + " °C\n" + "Regnrisk: " + rain + "\n" + "Totalvikt ca: "
-                /*+ String.format(Locale.ENGLISH, "%.2f", totalWeight)*/;
+                + String.format(Locale.ENGLISH, "%.2f", totalWeight);
     }
 }
